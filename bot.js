cfece9601871a19dda11b8f97308a2358f75445b
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-08-12 19:39:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -262,5 +262,20 @@ client.on('message', function(message) {
 
 
 
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('*cb')){
+ if(!message.author.id === '244888652004458497') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
